eef5eec725eb2368cdc3d09c93658c5eedf368c6
Federico Builes <federico.builes@gmail.com>
2011-02-07 22:27:17 -0500

    Rescue from YAML errors.
    * In Ruby 1.9.2 YAML will fail with `ArgumentError` when it
    receives the Github post-receive POST. This should fix it.

--- lib/cijoe/server.rb
+++ lib/cijoe/server.rb
@@ -27,7 +27,8 @@ class CIJoe
     end
 
     post '/?' do
-      payload = YAML.load(params[:payload].to_s) || {}
+      payload = YAML.load(params[:payload].to_s) rescue { }
+      payload ||= { }
       pushed_branch = payload['ref'].to_s.split('/').last
 
       # Only build if we were given an explicit branch via `?branch=blah`,
